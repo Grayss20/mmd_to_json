2bf848bef88d570383da72cac161b4e7e08df904
Grayss20 <157813957+Grayss20@users.noreply.github.com>
2024-10-07 14:17:24 +0100

    1

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from pprint import pprint
 from utils import read_file
 from extractors import extract_questions, remove_numeric_placeholders
 
-# Example usage
 if __name__ == "__main__":
     file_path = "to_proceed/que/9ma0-01-que-20220608.mmd"
     content = read_file(file_path)
